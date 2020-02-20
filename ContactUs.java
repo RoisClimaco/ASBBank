de128fb0677105ae690bf1abf98e6c6a216a2c95
RoisClimaco <53389410+RoisClimaco@users.noreply.github.com>
2020-02-21 09:29:40 +1300

    Update ContactUs.java

--- ContactUs.java
+++ ContactUs.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class ContactUs {
     public static void main (String[] args) {
 	System.out.println ("This is the Contact Us Page!");
 	System.out.println ("Email us anytime and we will try to respond immediately");
